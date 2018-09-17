11721925dcb6732706821a894d46ef4011b9c867
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-17 14:43:58 -0700

    done the update attack

--- superheros.py
+++ superheros.py
@@ -4,16 +4,14 @@ import random
 
      def __init__(self, name, attack_strength):
          self.name = name
-         self.attack_strength = strength
+         self.attack_strength = attack_strength
 
 
      def attack(self):
-         lowest_possible_attack = attack_strength // 2
-         random.randint(lowest_possible_attack,attack_strength)
+         lowest_possible_attack = self.attack_strength // 2
+         random.randint(lowest_possible_attack,self.attack_strength)
 
 
 
-     def update_attack(self, attack_strength):
-
-
-abilty_name = Ability("Punch")
+     def update_attack(self, new_strength):
+         self.attack_strength = new_strength
