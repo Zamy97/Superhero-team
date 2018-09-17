852e8a36013bd6e25ebaab848e857ab47a638232
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-17 13:57:55 -0700

    working on an ability class

---
+++ superheros.py
@@ -0,0 +1,12 @@
+import random
+
+ class Ability:
+
+     def __init__(self, name, attack_strength):
+         self.name = name
+         self.attack_strength = strength
+
+
+     def attack(self):
+
+     def update_attack(self, attack_strength):
