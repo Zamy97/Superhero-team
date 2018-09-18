49b4035270260d68fddc68cdecea41e8fcc7f50a
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-17 17:27:01 -0700

    ability stuff works

--- superheros.py
+++ superheros.py
@@ -9,7 +9,8 @@ class Ability:
 
      def attack(self):
          lowest_possible_attack = self.attack_strength // 2
-         random.randint(lowest_possible_attack,self.attack_strength)
+         attack = random.randint(lowest_possible_attack,self.attack_strength)
+         return attack
 
 
 
@@ -24,20 +25,21 @@ class Hero:
 
 
     def add_ability(self, ability):
-        abilities.append(ability)
+        self.abilities.append(ability)
 
     def attack(self):
+
         total = 0
         for each_items in self.abilities:
             total += each_items.attack()
         return total
 
-
+if __name__ == "__main__":
 hero = Hero("Spider Man")
 print(hero.attack())
-ability = Ability("Divine speed", 300)
+ability = Ability("Jumps", 300)
 hero.add_ability(ability)
 print(hero.attack())
-new_ability = Ability("Super Human Strength", 800)
+new_ability = Ability("Web Power", 500)
 hero.add_ability(new_ability)
 print(hero.attack())
