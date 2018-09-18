22f0799824d88df446b703bc84738a321a969e45
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-17 17:53:13 -0700

    learning inheritence

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-class Cat:
+# class Cat:
+#
+#     def crying(self):
+#         print("Meow!")
+#
+# my_cats = list()
+#
+# my_cats.append(Cat())
+# my_cats.append(Cat())
+#
+# for cat in my_cats:
+#     cat.crying()
 
-    def crying(self):
-        print("Meow!")
-
-my_cats = list()
-
-my_cats.append(Cat())
-my_cats.append(Cat())
-
-for cat in my_cats:
-    cat.crying()
+# class Cat:
+#
+#     def crying(self):
+#         print("Meow!")
+#
+# my_cats = Cat()
+# my_cats.crying()
