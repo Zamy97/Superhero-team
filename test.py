4940177e9aa601f573235ec3d79d1dd2ca8956f0
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-17 21:16:14 -0700

    adding an animal function

--- test.py
+++ test.py
@@ -21,15 +21,28 @@
 
 
 
-class Animal:
+# class Animal:
+#
+#     def __init__(self, name, sleep_time):
+#         self.name = name
+#         self.sleep_time = sleep_time
+#
+#     def sleep(self):
+#         print("{} sleeps for {} hours.".format(
+#              self.name, self.sleep_time ))
+#
+# kiddo = Animal("Spot", 16)
+# kiddo.sleep()
 
+class Animal:
     def __init__(self, name, sleep_time):
         self.name = name
         self.sleep_time = sleep_time
 
     def sleep(self):
-        print("{} sleeps for {} hours.".format(
-             self.name, self.sleep_time ))
+        print(
+        "{} slepps for {} hours.".format(
+        self.name, self.sleep_time))
 
-kiddo = Animal("Spot", 16)
-kiddo.sleep()
+    def bark(self):
+        
