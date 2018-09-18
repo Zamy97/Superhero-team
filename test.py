6763e4008301312dc7db18f3642b50beb5c82b3b
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-17 18:09:50 -0700

    learing inheritence

--- test.py
+++ test.py
@@ -18,3 +18,18 @@
 #
 # my_cats = Cat()
 # my_cats.crying()
+
+
+
+class Animal:
+
+    def __init__(self, name, sleep_time):
+        self.name = name
+        self.sleep_time = sleep_time
+
+    def sleep(self):
+        print("{} sleeps for {} hours.".format(
+             self.name, self.sleep_time ))
+
+kiddo = Animal("Spot", 16)
+kiddo.sleep()
