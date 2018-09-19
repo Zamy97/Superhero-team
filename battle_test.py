1f9daca8e26766b22942c8b420fd5ee0f0eded8b
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-18 23:28:16 -0700

    adding some tests to see if the methods works

---
+++ battle_test.py
@@ -0,0 +1,20 @@
+import pytest
+import superheroes
+import random
+import io
+import sys
+
+# Helper Functions
+
+
+def capture_console_output(function_body):
+    # _io.StringIO object
+    string_io = io.StringIO()
+    sys.stdout = string_io
+    function_body()
+    sys.stdout = sys.__stdout__
+    return string_io.getvalue()
+
+
+def create_armor():
+    armors = [
