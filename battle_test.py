f0064dd18ccf5f62fb2ef67138c85c5c714aadb8
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-18 23:30:30 -0700

    Calculator,

--- battle_test.py
+++ battle_test.py
@@ -18,3 +18,43 @@ def capture_console_output(function_body):
 
 def create_armor():
     armors = [
+    "Calculator",
+        "Laser Shield",
+        "Invisibility",
+        "SFPD Strike Force",
+        "Social Workers",
+        "Face Paint",
+        "Damaskus Shield",
+        "Bamboo Wall",
+        "Forced Projection",
+        "Thick Fog",
+        "Wall of Will",
+        "Wall of Walls",
+        "Obamacare",
+        "Thick Goo"]
+    name = armors[random.randint(0, len(armors) - 1)]
+    power = random.randint(23, 700000)
+    return superheroes.Armor(name, power)
+
+
+def create_weapon():
+    weapons = [
+        "Antimatter Gun",
+        "Star Cannon",
+        "Black Hole Ram Jet",
+        "Laser Sword",
+        "Laser Cannon",
+        "Ion Accellerated Disc Drive",
+        "Superhuman Strength",
+        "Blinding Lights",
+        "Ferociousness",
+        "Speed of Hermes",
+        "Lightning Bolts"]
+    name = weapons[random.randint(0, len(weapons) - 1)]
+    power = random.randint(27, 700000)
+    return superheroes.Weapon(name, power)
+
+
+def create_ability():
+    abilities = [
+        "Alien Attack",
