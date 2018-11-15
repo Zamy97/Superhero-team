bf3942e1b9b72df1051bbd8fbb14b4ba4d5ecaa1
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-15 15:40:40 -0800

    going to be starting page two soon

--- dog.py
+++ dog.py
@@ -1,15 +1,23 @@
 
 class Dog:
-    greeting = "woof!"
+    greeting = "Hello there"
 
     def __init__(self, name):
         self.name = name
 
-
-
     def bark(self):
         print(self.greeting)
 
+if __name__ == "__main__":
+    my_first_dog = Dog("Spot")
+    my_first_dog.bark()
+
+print(my_first_dog.name)
+
+    # greeting = "woof!"
+    #
+    # def __init__(self, name):
+    #     self.name = name
 
 # if __name__ == "__main__":
 #     my_dog = Dog("Spot")
@@ -17,11 +25,11 @@ class Dog:
 #     print(my_dog.name)
 #     print(my_other_dog.name)
 
-my_first_dog = Dog("Annie")
-my_second_dog = Dog("Wyatt")
+# my_first_dog = Dog("Annie")
+# my_second_dog = Dog("Wyatt")
 
-print(my_first_dog.name)
-print(my_second_dog.name)
-
-my_first_dog.bark()
-my_second_dog.bark()
+# print(my_first_dog.name)
+# print(my_second_dog.name)
+#
+# my_first_dog.bark()
+# my_second_dog.bark()
